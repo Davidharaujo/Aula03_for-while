524096dd6231eebe0e1282d0f96f624280601293
David Henrique Araujo <95188952+Davidharaujo@users.noreply.github.com>
2022-06-17 11:07:45 -0300

    exercicio resolvido

---
+++ script.js
@@ -0,0 +1,18 @@
+let indice=1
+
+for(i=1; i<=10; i++) {
+    console.log(i)
+}
+
+while ( indice<=10 ) {
+    console.log(indice)
+    indice++;
+}
+
+for(i=1; i<=100; i+=2) {
+    console.log(i)
+}
+
+for(i=0; i<=100; i+=2) {
+    console.log(i)
+}
